88696fa70043d2711799038c2287a9a8b4e4e2a4
ZifanWu <91784718+ZifanWu@users.noreply.github.com>
2022-03-26 12:47:19 +0800

    Update train_smac.sh

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -12,6 +12,6 @@ do
     CUDA_VISIBLE_DEVICES=1 \
     python train/train_smac.py --n_training_threads 23 --n_rollout_threads 8  --num_env_steps 2000000 --ppo_epoch 15 --num_mini_batch 1 --episode_length 400 --gamma 0.99 --gae_lambda 0.90 \
     --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map}\
-    --use_value_active_masks --add_center_xy --use_state_agent --add_agent_id --use_adv --use_recurrent_policy \
+    --use_value_active_masks --add_center_xy --use_state_agent --add_agent_id --use_recurrent_policy \
     --use_eval
 done
